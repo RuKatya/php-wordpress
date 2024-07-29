90263c9a3647f9e41f5af891c99d784fe424ef77
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2024-07-29 23:17:12 +0300

    first

---
+++ first-lessaon/test.php
@@ -0,0 +1,37 @@
+<?php
+
+error_reporting(-1);
+
+echo "This is test epta<br>";
+// echo "This is test epta";
+# erfgergerg
+print "wewefwef<br>";
+
+/**
+ * wergergerg
+ * e
+ * rger
+ * g
+ * erg
+ * er
+ * ge
+ * rg
+ * er
+ * e
+ * gr
+ * g
+ */
+
+
+$epta = "epta";
+echo "namemeeee\":\" {$epta}sssss<br>";
+ $epta = "ept3423423424a";
+ echo "namemeeee: $epta<br>";
+
+ define("huina","che-za-dich");
+
+ echo "huina<br>";
+
+ define("echo", "ajajajajaja");
+
+ echo constant("echo");
